f06a89d3784c1f5c31ffd2caa30ed2d1c3706707
ashish.gehani@gmail.com <ashish.gehani@gmail.com@fb02b236-a367-3961-9825-d18e7369a4b9>
2011-03-16 23:35:41 +0000

    Reporter to audit TCP/UDP network connections.
    Open connections logged with 'lsof' every second.

---
+++ trunk/SPADE/src/spade/reporter/Lsof.java
@@ -0,0 +1,311 @@
+/*
+--------------------------------------------------------------------------------
+SPADE - Support for Provenance Auditing in Distributed Environments.
+Copyright (C) 2011 SRI International
+
+This program is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+--------------------------------------------------------------------------------
+ */
+
+package spade.reporter;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.InetAddress;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+
+import spade.core.AbstractReporter;
+import spade.opm.edge.Used;
+import spade.opm.edge.WasGeneratedBy;
+import spade.opm.vertex.Artifact;
+
+public class Lsof extends AbstractReporter implements Runnable{
+
+	PrintStream outputStream = System.out;
+	PrintStream errorStream = System.err;
+	java.lang.Process process;
+	volatile boolean shutdown = false;
+	
+	HashMap<String,HashSet<String>> currentPidConnectionMap = 
+		new HashMap<String,HashSet<String>>();
+	HashMap<String,HashSet<String>> nextPidConnectionMap =
+		new HashMap<String,HashSet<String>>();
+	
+    @Override
+    public boolean launch(String arguments) {
+
+		boolean started = false;
+		
+		try{
+			new Thread(new Lsof()).start();
+			started = true;
+		}
+		catch(Exception exception){
+			exception.printStackTrace(errorStream);
+		}
+		return started;
+    }
+
+	public void run(){
+		
+		BufferedReader lsofBufferedReader = initialize();
+		
+		if(lsofBufferedReader != null){
+			while(!shutdown){
+				scanList(lsofBufferedReader);
+			}
+		}
+	}
+
+	BufferedReader initialize(){
+		
+		BufferedReader lsofBufferedReader = null;
+		
+		try{
+			process = Runtime.getRuntime().exec("lsof -F pn -i -r 1");
+			InputStream inputStream = process.getInputStream();
+			InputStreamReader inputStreamReader =
+				new InputStreamReader(inputStream);
+			lsofBufferedReader = new BufferedReader(inputStreamReader);
+		}
+		catch(Exception exception){
+			exception.printStackTrace(errorStream);
+		}
+		
+		return lsofBufferedReader;
+	}
+	
+	void scanList(BufferedReader lsofBufferedReader){
+		
+		String pid;
+		boolean done = false;
+		
+		do{
+			pid = scanProcess(lsofBufferedReader);
+			
+			if(pid != null){
+				scanConnections(lsofBufferedReader, pid);
+			}
+			
+			if(peekString(lsofBufferedReader, "m")){
+				
+				currentPidConnectionMap = nextPidConnectionMap;
+				nextPidConnectionMap = new HashMap<String,HashSet<String>>();
+				try{
+					lsofBufferedReader.readLine();
+				}
+				catch(Exception exception){
+					exception.printStackTrace(errorStream);
+				}
+				done = true;
+			}
+			
+		} while(!done);
+	}
+
+	String scanProcess(BufferedReader lsofBufferedReader){
+		
+		String pid = null;
+		
+		try{
+			HashSet<String> connectionSet;
+			String line;
+			
+			line = lsofBufferedReader.readLine();
+			
+			if(line.startsWith("p")){
+				pid = line.substring(1);
+				
+				if(currentPidConnectionMap.containsKey(pid)){
+					connectionSet = currentPidConnectionMap.get(pid);
+				}else{
+					connectionSet = new HashSet<String>();
+				}
+				nextPidConnectionMap.put(pid, connectionSet);
+			}else{
+				errorStream.println("Expected line to start with 'p':\n" +
+									"\t" + line);
+			}
+		}
+		catch(Exception exception){
+			exception.printStackTrace(errorStream);
+		}
+		
+		return pid;
+	}
+	
+	void scanConnections(BufferedReader lsofBufferedReader, String pid){
+		
+		try{
+			String line, connection;
+			HashSet<String> connectionSet;
+			boolean done = false;
+
+			connectionSet = nextPidConnectionMap.get(pid);
+			do{
+				if(peekString(lsofBufferedReader, "n")){
+					
+					line = lsofBufferedReader.readLine();
+					connection = line.substring(1);
+					
+					if(connection.matches(".*:.*->.*:.*")){
+						if(!connectionSet.contains(connection)){
+							connectionSet.add(connection);
+							emitOPM(pid, connection);
+						}
+					}
+				}else{
+					done = true;
+				}
+			} while(!done);
+		}
+		catch(Exception exception){
+			exception.printStackTrace(errorStream);
+		}
+	}
+
+	void emitString(String pid, String connection){
+		outputStream.println("PID: " + pid + "\t\tConnection: " + connection);
+	}
+	
+	void emitOPM(String pid, String connection){
+		
+		try{
+			String source, destination, port;
+			String[] endPoint, endPoints;
+			LinkedHashMap annotations;
+			boolean endPointMatched = false;
+			Date currentTime;
+			
+			spade.opm.vertex.Process processVertex;
+			Artifact networkVertex;
+			WasGeneratedBy wasGeneratedByEdge;
+			Used usedEdge;
+
+			// Create process vertex.
+			annotations = new LinkedHashMap();
+			annotations.put("pid", pid);
+			processVertex = new spade.opm.vertex.Process(annotations);
+			
+			if(!putVertex(processVertex)){
+				errorStream.println("Buffer did not accept process artifact:" +
+									"\n\t pid" + pid);
+			}
+							
+			// Create network artifact.
+			annotations = new LinkedHashMap();
+			endPoints = connection.split("->");
+			
+			endPoint = endPoints[0].split(":");
+			source = endPoint[0];
+			annotations.put("source host", source);
+			port = endPoint[1];
+			annotations.put("source port", port);
+			
+			endPoint = endPoints[1].split(":");
+			destination = endPoint[0];
+			annotations.put("destination host", destination);
+			port = endPoint[1];
+			annotations.put("destination port", port);
+			
+			networkVertex = new Artifact(annotations);
+			if(!putVertex(networkVertex)){
+				errorStream.println("Buffer did not accept connection artifact:"
+									+ "\n\t " + connection);
+			}
+			
+			// Create an outgoing edge.
+			if(InetAddress.getByName(destination).isSiteLocalAddress()){
+				annotations = new LinkedHashMap();
+				currentTime = new Date();
+				annotations.put("time", currentTime.toString()); 
+				usedEdge = new Used(processVertex, networkVertex, annotations);
+				if(!putEdge(usedEdge)){
+					errorStream.println("Buffer did not accept outgoing " +
+										"connection edge:\n\t pid: " + pid +
+										"\n\t connection: " + connection +
+										"\n\t time: " + currentTime.toString());
+				}
+				endPointMatched = true;
+			}
+
+			// Create an incoming edge.
+			if(InetAddress.getByName(source).isSiteLocalAddress()){
+				annotations = new LinkedHashMap();
+				currentTime = new Date();
+				annotations.put("time", currentTime.toString()); 
+				wasGeneratedByEdge =
+					new WasGeneratedBy(networkVertex, processVertex,
+									   annotations);
+				if(!putEdge(wasGeneratedByEdge)){
+					errorStream.println("Buffer did not accept incoming " +
+										"connection edge:\n\t pid: " + pid +
+										"\n\t connection: " + connection +
+										"\n\t time: " + currentTime.toString());
+				}
+
+				endPointMatched = true;
+			}
+			
+			if(!endPointMatched){
+				errorStream.println("Neither endpoint is local:\n\t" + 
+									connection);
+			}
+		}
+		catch(Exception exception){
+			exception.printStackTrace(errorStream);
+		}
+	}
+	
+	boolean peekString(BufferedReader bufferedReader, String nextString){
+		
+		boolean match = false;
+		
+		try{
+			int charactersRead, nextStringLength = nextString.length();
+			char[] peekBuffer = new char[nextStringLength];
+			String peekString;
+			
+			bufferedReader.mark(nextStringLength);
+			charactersRead = bufferedReader.read(peekBuffer, 0, 
+												 nextStringLength);
+			bufferedReader.reset();
+			
+			if(charactersRead == nextStringLength){
+				peekString = new String(peekBuffer);
+				if(peekString.equals(nextString)){
+					match = true;
+				}
+			}
+		}
+		catch(Exception exception){
+			exception.printStackTrace(errorStream);
+		}
+		
+		return match;
+	}
+	
+    @Override
+    public boolean shutdown() {
+		
+		shutdown = true;
+		process.destroy();
+		return true;
+    }
+}
