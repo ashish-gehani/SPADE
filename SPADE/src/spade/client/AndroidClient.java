718cf35616e37a2b2403570c0a18b2cd5bbe23be
dawood.tariq <dawood.tariq@gmail.com>
2012-02-11 08:56:46 +0000

    Updated AndroidClient to resolve thread race conditions.

--- SPADE/src/spade/client/AndroidClient.java
+++ SPADE/src/spade/client/AndroidClient.java
@@ -1,21 +1,21 @@
 /*
---------------------------------------------------------------------------------
-SPADE - Support for Provenance Auditing in Distributed Environments.
-Copyright (C) 2011 SRI International
+ --------------------------------------------------------------------------------
+ SPADE - Support for Provenance Auditing in Distributed Environments.
+ Copyright (C) 2011 SRI International
 
-This program is free software: you can redistribute it and/or
-modify it under the terms of the GNU General Public License as
-published by the Free Software Foundation, either version 3 of the
-License, or (at your option) any later version.
+ This program is free software: you can redistribute it and/or
+ modify it under the terms of the GNU General Public License as
+ published by the Free Software Foundation, either version 3 of the
+ License, or (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
---------------------------------------------------------------------------------
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------------
  */
 package spade.client;
 
@@ -31,6 +31,7 @@ public class AndroidClient {
     private static PrintStream errorStream;
     private static PrintStream SPADEControlIn;
     private static BufferedReader SPADEControlOut;
+    private static BufferedReader commandReader = new BufferedReader(new InputStreamReader(System.in));
     private static volatile boolean shutdown;
     private static final String COMMAND_PROMPT = "-> ";
     private static final int THREAD_SLEEP_DELAY = 10;
@@ -46,14 +47,6 @@ public class AndroidClient {
 
             public void run() {
                 try {
-                    SocketAddress sockaddr = new InetSocketAddress("localhost", Kernel.LOCAL_CONTROL_PORT);
-                    Socket remoteSocket = new Socket();
-                    remoteSocket.connect(sockaddr, Kernel.CONNECTION_TIMEOUT);
-                    OutputStream outStream = remoteSocket.getOutputStream();
-                    InputStream inStream = remoteSocket.getInputStream();
-                    SPADEControlOut = new BufferedReader(new InputStreamReader(inStream));
-                    SPADEControlIn = new PrintStream(outStream);
-            
                     while (!shutdown) {
                         if (SPADEControlOut.ready()) {
                             // This thread keeps reading from the output pipe and
@@ -76,19 +69,24 @@ public class AndroidClient {
                 }
             }
         };
-        new Thread(outputReader).start();
 
         try {
+            SocketAddress sockaddr = new InetSocketAddress("localhost", Kernel.LOCAL_CONTROL_PORT);
+            Socket remoteSocket = new Socket();
+            remoteSocket.connect(sockaddr, Kernel.CONNECTION_TIMEOUT);
+            OutputStream outStream = remoteSocket.getOutputStream();
+            InputStream inStream = remoteSocket.getInputStream();
+            SPADEControlOut = new BufferedReader(new InputStreamReader(inStream));
+            SPADEControlIn = new PrintStream(outStream);
+            new Thread(outputReader).start();
 
             outputStream.println("");
             outputStream.println("SPADE 2.0 Control Client for Android");
             outputStream.println("");
-            
-            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
             SPADEControlIn.println("");
             while (true) {
-                String line = in.readLine();
+                String line = commandReader.readLine();
                 if (line.split("\\s")[0].equalsIgnoreCase("query")) {
                     // Do not allow query commands from this control shell.
                     SPADEControlIn.println("");
