05f6706b42c33c2a05968095bcc4b29e5de68283
ian.alistair.mason <ian.alistair.mason@gmail.com>
2012-03-16 16:47:22 +0000

    Added some informative comments.

--- SPADE/src/spade/reporter/close.c
+++ SPADE/src/spade/reporter/close.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
+/* will hide this version of close when we link close.o with the "host" program  into a shared library */
 #define HIDDEN __attribute__((visibility("hidden"))) 
 
 int llvm_close(int close);
 
 HIDDEN int close(int fd){
   return llvm_close(fd);
-  return 0;
 }
