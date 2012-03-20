0beb7458d8b776167d3a58a3d13277d3a010a856
ian.alistair.mason <ian.alistair.mason@gmail.com>
2012-03-20 17:56:39 +0000

    Being careful, merging in the new thread stuff, IV

--- SPADE/src/spade/reporter/LLVMReporterLib.c
+++ SPADE/src/spade/reporter/LLVMReporterLib.c
@@ -8,10 +8,18 @@
 #include <unistd.h>
 #include <errno.h>
 
+#if defined(_LLVMREPORTER_MACOSX)
+#include <pthread.h>
+#elif defined(_LLVMREPORTER_LINUX)
+#include <sys/syscall.h>
+#elif defined(_LLVMREPORTER_WINDOWS)
+//Ian says: let me know if you want windows...
+#endif
+
 int LLVMSocket = 5000;
 
 /* turn this on to use independently of the spade server; reporting goes to stderr */
-#define _IANS_DEBUGGING  0
+#define _IANS_DEBUGGING  1
 
 #if _IANS_DEBUGGING == 1
 int sock = STDERR_FILENO;
@@ -21,21 +29,35 @@ int sock = -1;
 
 FILE* socket_fp = NULL;
 
-/*
- *  Ian says: still need to got a  get_thread_id() routine that works for all platforms.
- *
- */
+unsigned long LLVMReporter_getThreadId(){
+  unsigned long retval = 0;
+#if defined(_LLVMREPORTER_MACOSX)
+  //look at pthread.h for this stuff; it's unique to a mac
+  //N.B. on my(all?) macs the value of pthread_self() is always the same for the main thread (regardless of which pid it has)
+  __uint64_t  tid;
+  pthread_t self = pthread_self();
+  pthread_threadid_np(self, &tid);
+  retval = tid;
+#elif defined(_LLVMREPORTER_LINUX)
+  retval = syscall(SYS_gettid);
+#elif defined(_LLVMREPORTER_WINDOWS)
+  retval =  0;
+#endif
+  //fprintf(stderr, "LLVMReporter_getThreadId(%lu)\n", retval);
+  return retval;
+}
 
 int llvm_close(int fd){
   if((sock != -1) && (sock == fd)){
     return 0;
   } else {
+    //don't close stderr
     return close(fd); 
   }
 }
 
 
-FILE* GetLLVMSocket(){
+FILE* LLVMReporter_getSocket(){
   if(socket_fp == NULL){
 
     if(sock == -1){
