1ade03fd0d0910255ace38ee69bc520530077c71
dawood.tariq <dawood.tariq@gmail.com>
2012-04-12 21:32:19 +0000

    Cleaned up LLVM build code: moved LLVM make instructions to the main makefile.

--- SPADE/src/spade/reporter/LLVMReporter.java
+++ SPADE/src/spade/reporter/LLVMReporter.java
@@ -1,22 +1,21 @@
-
 /*
---------------------------------------------------------------------------------
-SPADE - Support for Provenance Auditing in Distributed Environments.
-Copyright (C) 2011 SRI International
+ --------------------------------------------------------------------------------
+ SPADE - Support for Provenance Auditing in Distributed Environments.
+ Copyright (C) 2011 SRI International
 
-This program is free software: you can redistribute it and/or
-modify it under the terms of the GNU General Public License as
-published by the Free Software Foundation, either version 3 of the
-License, or (at your option) any later version.
+ This program is free software: you can redistribute it and/or
+ modify it under the terms of the GNU General Public License as
+ published by the Free Software Foundation, either version 3 of the
+ License, or (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
---------------------------------------------------------------------------------
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------------
  */
 package spade.reporter;
 
@@ -24,7 +23,10 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import spade.core.AbstractEdge;
@@ -36,7 +38,6 @@ import spade.edge.opm.WasTriggeredBy;
 import spade.vertex.opm.Artifact;
 import spade.vertex.opm.Process;
 
-
 public class LLVMReporter extends AbstractReporter {
 
     public static volatile boolean shutdown;
@@ -45,7 +46,7 @@ public class LLVMReporter extends AbstractReporter {
     public static final int THREAD_SLEEP_DELAY = 500;
     public static LLVMReporter Reporter = null;
     public final int SocketNumber = 5000;
-    
+
     @Override
     public boolean launch(String arguments) {
         Reporter = this;
@@ -59,17 +60,17 @@ public class LLVMReporter extends AbstractReporter {
         //
         functionStackMap = Collections.synchronizedMap(new HashMap<String, Stack>());
         try {
-			// Create connectionThread to listen on the socket and create EventHandlers
+            // Create connectionThread to listen on the socket and create EventHandlers
             Runnable connectionThread = new Runnable() {
+
                 public void run() {
-                    while(!shutdown) {
+                    while (!shutdown) {
                         try {
                             Socket connected = Server.accept();
                             EventHandler eventHandler = new EventHandler(connected);
                             new Thread(eventHandler).start();
                             Thread.sleep(THREAD_SLEEP_DELAY);
-                        }
-                        catch (Exception exception){
+                        } catch (Exception exception) {
                             exception.printStackTrace(System.err);
                         }
                     }
@@ -90,18 +91,17 @@ public class LLVMReporter extends AbstractReporter {
     }
 }
 
+class EventHandler implements Runnable {
 
-class EventHandler implements Runnable{
     Socket threadSocket;
     int FunctionId = 0;
 
     EventHandler(Socket socket) {
         threadSocket = socket;
     }
-    
+
     @Override
-    public void run()
-    {
+    public void run() {
         try {
             BufferedReader inFromClient = new BufferedReader(new InputStreamReader(threadSocket.getInputStream()));
             while (!LLVMReporter.shutdown) {
@@ -118,32 +118,30 @@ class EventHandler implements Runnable{
             exception.printStackTrace(System.err);
         }
     }
-    
+
     //trace contains thread id, function entry or exit, function name and arguments or return value.
-    //trace looks like "123 E: $foo Arg #0: i32 %a =123". 
+    //trace looks like "123 E: $foo Arg #0: i32 %a =123".
     private void parseEvent(String line) {
         try {
-            AbstractVertex function = null;
-            AbstractVertex argument = null;
-            AbstractEdge edge = null;
-            LinkedHashMap<String, String> annotations = new LinkedHashMap<String, String>();
+            AbstractVertex function;
+            AbstractVertex argument;
+            AbstractEdge edge;
             if (line.length() > 0) {
-				// get thread id
-                String tid = "";
-                
+                // get thread id
+                String tid;
+
                 int index = line.indexOf(' ');
                 tid = line.substring(0, index);
-                
+
                 // if the functionStackMap does not contain a stack for that thread, create a new stack.
-                if (!LLVMReporter.Reporter.functionStackMap.containsKey(tid))
-                {
+                if (!LLVMReporter.Reporter.functionStackMap.containsKey(tid)) {
                     LLVMReporter.Reporter.functionStackMap.put(tid, new Stack());
                 }
                 //Gets EventType - Entry or Exit
                 line = line.substring(index + 1);
                 char EventType = line.charAt(0); //EventType indicates entering or returning of a function
                 line = line.substring(4); // The rest of the line is function name and arguments/return value.
-                
+
                 //Get the function Name
                 String functionName;
                 index = line.indexOf(' ');
@@ -152,38 +150,38 @@ class EventHandler implements Runnable{
                 } else { // if there are no arguments and no return value
                     functionName = line;
                 }
-                
+
                 Pattern pattern;
                 if (EventType == 'E') {
-					// Expecting Argument number, arg type, arg name and arg value. eg: Arg #0: i32 %a =123
+                    // Expecting Argument number, arg type, arg name and arg value. eg: Arg #0: i32 %a =123
                     pattern = Pattern.compile("Arg #([0-9]+): ([^ ]+) %([^ ]+) =([^ ]+)");
                     Matcher items = pattern.matcher(line);
-                    
+
                     function = new Process();
                     // process id is a combination of functionName, functionId, and thread ID
                     function.addAnnotation("pid", functionName + "." + FunctionId + "." + tid);
                     function.addAnnotation("name", functionName);
                     function.addAnnotation("tid", tid);
-                    
+
                     LLVMReporter.Reporter.putVertex(function);
                     while (items.find()) {
                         argument = new Artifact();
-                        
+
                         String ArgNo = items.group(1);
                         // id is a combination of functionName, functionId and Argument Number
                         argument.addAnnotation("ID", functionName + "." + FunctionId + "-" + ArgNo);
-                        
+
                         String ArgType = items.group(2);
                         argument.addAnnotation("ArgType", ArgType);
-                        
+
                         String ArgName = items.group(3);
                         argument.addAnnotation("ArgName", ArgName);
-                        
+
                         String ArgVal = items.group(4);
                         argument.addAnnotation("ArgVal", ArgVal);
-                        
+
                         LLVMReporter.Reporter.putVertex(argument);
-                        
+
                         if (!LLVMReporter.Reporter.functionStackMap.get(tid).empty()) {
                             edge = new WasGeneratedBy((Artifact) argument, (Process) LLVMReporter.Reporter.functionStackMap.get(tid).peek());
                             LLVMReporter.Reporter.putEdge(edge);
@@ -199,22 +197,22 @@ class EventHandler implements Runnable{
                     FunctionId++;
                 } else // in case of EventType being Return
                 {
-					// Expecting ret type, ret name and ret value. "R:  i32 %ret =2". Ret name is ignored
+                    // Expecting ret type, ret name and ret value. "R:  i32 %ret =2". Ret name is ignored
                     pattern = Pattern.compile("R:  ([^ ]+) %([^ ]+) =(.+)");
                     Matcher items = pattern.matcher(line);
                     if (items.find()) {
                         argument = new Artifact();
                         String RetType = items.group(1);
                         argument.addAnnotation("ReturnType", RetType);
-                        
+
                         String RetVal = items.group(3);
                         argument.addAnnotation("ReturnVal", RetVal);
-                        
+
                         LLVMReporter.Reporter.putVertex(argument);
                         edge = new WasGeneratedBy((Artifact) argument, (Process) LLVMReporter.Reporter.functionStackMap.get(tid).peek());
                         LLVMReporter.Reporter.putEdge(edge);
-                        LLVMReporter.Reporter.functionStackMap.get(tid).pop();
                     }
+                    LLVMReporter.Reporter.functionStackMap.get(tid).pop();
                 }
 
             }
@@ -223,5 +221,4 @@ class EventHandler implements Runnable{
             exception.printStackTrace(System.err);
         }
     }
-
 }
