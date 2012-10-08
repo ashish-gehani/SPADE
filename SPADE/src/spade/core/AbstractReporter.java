e289b400042867e35271ab3a4ea4eac22fd330da
sharjeel.qureshi <sharjeel.qureshi@gmail.com>
2012-10-08 17:13:54 +0000

    Updated Makefile

--- SPADE/src/spade/core/AbstractReporter.java
+++ SPADE/src/spade/core/AbstractReporter.java
@@ -19,6 +19,8 @@
  */
 package spade.core;
 
+import org.apache.log4j.Logger;
+
 /**
  * This is the base class for reporters.
  *
@@ -38,7 +40,7 @@ public abstract class AbstractReporter {
      * @param buffer The buffer to be set for this reporter.
      */
     public final void setBuffer(Buffer buffer) {
-        internalBuffer = buffer;
+    	internalBuffer = buffer;
     }
 
     /**
--- SPADE/src/spade/reporter/DOT.java
+++ SPADE/src/spade/reporter/DOT.java
@@ -20,9 +20,15 @@
 package spade.reporter;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
@@ -42,7 +48,7 @@ import spade.vertex.opm.Process;
 public class DOT extends AbstractReporter {
 
     private BufferedReader eventReader;
-    private volatile boolean shutdown = false;
+    protected volatile boolean shutdown = false;
     private Map<String, AbstractVertex> vertexMap = new HashMap<String, AbstractVertex>();
     static final Logger logger = Logger.getLogger(Audit.class.getName());
     // Pattern to match nodes
@@ -56,6 +62,7 @@ public class DOT extends AbstractReporter {
             return false;
         }
         try {
+        	logger.info("Opening file '" + arguments + "' for reading");
             eventReader = new BufferedReader(new FileReader(arguments));
         } catch (Exception exception) {
             logger.log(Level.SEVERE, null, exception);
@@ -82,7 +89,7 @@ public class DOT extends AbstractReporter {
         new Thread(lineProcessor, "DOT-Thread").start();
         return true;
     }
-
+    
     @Override
     public boolean shutdown() {
         shutdown = true;
