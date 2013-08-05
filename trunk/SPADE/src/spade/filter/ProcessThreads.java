d4963edb14b02098369d88843b0fac562cd482e9
sharjeel.qureshi@gmail.com <sharjeel.qureshi@gmail.com@fb02b236-a367-3961-9825-d18e7369a4b9>
2013-08-05 23:31:44 +0000

    Fixes orphan threads with no main process seen

--- trunk/SPADE/src/spade/filter/ProcessThreads.java
+++ trunk/SPADE/src/spade/filter/ProcessThreads.java
@@ -17,236 +17,281 @@ import spade.edge.opm.WasGeneratedBy;
 import spade.vertex.opm.Artifact;
 import spade.vertex.opm.Process;
 
-
 /**
- * This filter groups together threads of same processes in one node
- * It might not scale well so ideally it should be used as post-processing i.e. re-run a dump through it
+ * This filter groups together threads of same processes in one node It might
+ * not scale well so ideally it should be used as post-processing i.e. re-run a
+ * dump through it
+ *
+ * Thread and Process nodes are held occasionally till they are deemed to be
+ * flushable Edges are always flushed at the end of processing during which they
+ * are remapped to new nodes
  *
  * @author Sharjeel Ahmed Qureshi
  */
-
 public class ProcessThreads extends AbstractFilter {
 
-	static final Logger logger = Logger.getLogger(ProcessThreads.class.getName());
+    static final Logger logger = Logger.getLogger(ProcessThreads.class.getName());
+
+    private class ShelvedProcess {
+
+        private AbstractVertex mainProcess = null;
+        private Map<String, Set<String>> multiAttributes = new HashMap<String, Set<String>>();
+
+        public ShelvedProcess(AbstractVertex vertex) {
+            mainProcess = vertex;
+            addAnotherVertexAnnotations(vertex);
+        }
+
+        public ShelvedProcess() {
+            mainProcess = null;
+        }
+
+        public void addAnotherVertexAnnotations(AbstractVertex vertex) {
+            assert vertex.type().equalsIgnoreCase("Process");
+            Map<String, String> annotations = vertex.getAnnotations();
+            for (String k : annotations.keySet()) {
+                // logger.log(Level.INFO, "Adding annotation k=" + k + " Value=" + annotations.get(k));
+                addAttribute(k, annotations.get(k));
+            }
+        }
+
+        public boolean isMainVertexSet() {
+            return mainProcess != null;
+        }
+
+        public void setMainProcess(AbstractVertex vertex) {
+            assert mainProcess == null;
+            mainProcess = vertex;
+            addAnotherVertexAnnotations(vertex);
+        }
+
+        public void addAttribute(String key, String value) {
+            if (!multiAttributes.containsKey(key)) {
+                multiAttributes.put(key, new HashSet<String>());
+            }
+            multiAttributes.get(key).add(value);
+        }
+
+        public AbstractVertex getVertex() {
+            // Converts all multi-attributes into one comma separated string, 
+            // adds it back in the original vertex and returns it
+
+            if (mainProcess == null) {
+                StringBuilder builder = new StringBuilder();
+                for (String k : multiAttributes.keySet()) {
+                    builder.append(k + "=");
+                    Set<String> attrs = multiAttributes.get(k);
+                    for (String a : attrs) {
+                        builder.append(a + ",");
+                    }
+                    builder.append(" ");
+                }
+                logger.log(Level.INFO, mainProcess.getAnnotation("pid"));  // Forcefully throough exception
+                logger.log(Level.WARNING, "Shelved vertex is null. All attributes: " + builder.toString());
+                return null;
+            }
 
 
-	private class ShelvedProcess {
+            for (String s : multiAttributes.keySet()) {
+                StringBuilder builder = new StringBuilder();
+                Set<String> attrsSet = multiAttributes.get(s);
 
-		private AbstractVertex mainProcess = null;
-		private Map<String, Set<String>> multiAttributes = new HashMap<String, Set<String>>();
+                if (s.equals("pid")) {
+                    attrsSet.remove(mainProcess.getAnnotation("pid"));
+                }
 
-		public ShelvedProcess(AbstractVertex vertex) {
-			mainProcess = vertex;
-			addAnotherVertexAnnotations(vertex);
-		}
+                ArrayList<String> attrs = new ArrayList<String>();
+                attrs.addAll(attrsSet);
 
-		public ShelvedProcess() {
-			mainProcess = null;
-		}
+                if (!attrs.isEmpty()) {
+                    builder.append(attrs.remove(0));
 
-		public void addAnotherVertexAnnotations(AbstractVertex vertex) {
-			assert vertex.type().equalsIgnoreCase("Process");
-			Map<String, String> annotations = vertex.getAnnotations();
-			for(String k : annotations.keySet() ) {
-				logger.log(Level.INFO, "Adding annotation k=" + k + " Value=" + annotations.get(k));
-				addAttribute( k, annotations.get(k) );
-			}
-		}
-
-		public boolean isMainVertexSet() {
-			return mainProcess != null;
-		}
-
-		public void setMainProcess(AbstractVertex vertex) {
-			assert mainProcess == null;
-			mainProcess = vertex;
-			addAnotherVertexAnnotations(vertex);
-		}
-
-		public void addAttribute(String key, String value) {
-			if (!multiAttributes.containsKey(key)) {
-				multiAttributes.put( key, new HashSet<String>() );
-			}
-			multiAttributes.get(key).add(value);
-		}
-
-		public AbstractVertex getVertex() {
-			// Converts all multi-attributes into one comma separated string, 
-			// adds it back in the original vertex and returns it
-
-			if (mainProcess == null) {
-				logger.log(Level.WARNING, "Shelved vertex is null");
-				return null;
-			}
+                    for (String attr : attrs) {
+                        builder.append(", ");
+                        builder.append(attr);
+                    }
+                }
 
 
-			for( String s: multiAttributes.keySet() ) {
-				StringBuilder builder = new StringBuilder();
-				Set<String> attrsSet = multiAttributes.get(s);
+                String strAttrs = builder.toString();
 
-				if (s.equals("pid")) 
-					attrsSet.remove(mainProcess.getAnnotation("pid"));
+                // logger.log(Level.INFO, "Setting attributes" + strAttrs);
 
-				ArrayList<String> attrs = new ArrayList<String>();
-				attrs.addAll(attrsSet);
+                if (s.equals("pid")) {
+                    // PID should be same as of original Process
+                    mainProcess.addAnnotation("tpids", strAttrs);
+                } else {
+                    mainProcess.addAnnotation(s, strAttrs);
+                }
+            }
 
-				if ( ! attrs.isEmpty() ) {
-					builder.append( attrs.remove(0) );
+            return mainProcess;
+        }
+    }
+    // Curernt active process node for merging threads, mapped by PID (String)
+    private Map<String, ShelvedProcess> currentMainProcessNode = new HashMap<String, ShelvedProcess>();
+    // Reference to flushed out processes
+    private Map<String, AbstractVertex> flushedOutVertices = new HashMap<String, AbstractVertex>();
+    private ArrayList<AbstractEdge> shelvedEdges = new ArrayList<AbstractEdge>();
+    private Set<AbstractVertex> shelvedThreads = new HashSet<AbstractVertex>();
 
-					for (String attr: attrs ) {
-						builder.append(", ");
-						builder.append(attr);
-					}
-				}
+    @Override
+    public void putVertex(AbstractVertex incomingVertex) {
 
+        if (incomingVertex.type().equalsIgnoreCase("EOS")) {
+            EOE();
+            return;
+        }
 
-				String strAttrs = builder.toString();
+        String dtgid = incomingVertex.getAnnotation("tgid");
+        if (dtgid != null && dtgid.equals("492"))
+        	logger.log(Level.INFO, "Processing vertex " + dtgid);
+        
+        if (incomingVertex.type().equalsIgnoreCase("Process")) {
 
-				logger.log(Level.INFO, "Setting attributes" + strAttrs);
+            String pid = incomingVertex.getAnnotation("pid");
+            String tgid = incomingVertex.getAnnotation("tgid");
+            
+            // logger.log(Level.INFO, "Processing PID:" + pid + " TGID:" + tgid);
 
-				if(s.equals("pid")) {
-					// PID should be same as of original Process
-					mainProcess.addAnnotation("tpids", strAttrs );
-				} else {
-					mainProcess.addAnnotation(s, strAttrs );
-				}
-			}
+            // Its a process
+            if (pid.equals(tgid)) {
 
-			return mainProcess;
-		}
-	}
-
-
-	// Curernt active process node for merging threads, mapped by PID (String)
-	private Map<String, ShelvedProcess> currentMainProcessNode = new HashMap<String, ShelvedProcess>(); 
-
-	// Reference to flushed out processes
-	private Map<String, AbstractVertex> flushedOutVertices = new HashMap<String, AbstractVertex>();
-
-	private ArrayList<AbstractEdge> shelvedEdges = new ArrayList<AbstractEdge>();
-	private Set<AbstractVertex> shelvedThreads = new HashSet<AbstractVertex>();
-
-	@Override
-	public void putVertex(AbstractVertex incomingVertex) {
-
-		if (incomingVertex.type().equalsIgnoreCase("EOS") ) {
-			EOE();
-			return;
-		}
-
-		if (incomingVertex.type().equalsIgnoreCase("Process")) {
-
-			String pid = incomingVertex.getAnnotation("pid");
-			String tgid = incomingVertex.getAnnotation("tgid");
-
-			// logger.info("Processing PID:" + pid + " TGID:" + tgid);
-
-			// Its a process
-			if ( pid.equals(tgid) ) {
-
-				// Remove the previous entry, if any
-				ShelvedProcess prevShelvedProcess = currentMainProcessNode.get(pid);
-				if ( prevShelvedProcess == null || (prevShelvedProcess != null && prevShelvedProcess.isMainVertexSet() ) )  {
-					flushVertex(pid);
-					// Shelf this vertex
-					currentMainProcessNode.put(pid, new ShelvedProcess(incomingVertex));
-					// logger.log(Level.INFO, "Process Shelved PID: " + pid);
-				} else {
-					// We've previously seen the thread but not the process. Now we're seeing the process so just register it.
-					assert !prevShelvedProcess.isMainVertexSet();
-					prevShelvedProcess.setMainProcess(incomingVertex);
-				}
+                // Remove the previous entry, if any
+                ShelvedProcess prevShelvedProcess = currentMainProcessNode.get(pid);
+                if (prevShelvedProcess == null || (prevShelvedProcess != null && prevShelvedProcess.isMainVertexSet())) {
+                    flushVertex(pid);
+                    // Shelf this vertex
+                    currentMainProcessNode.put(pid, new ShelvedProcess(incomingVertex));
+                    // logger.log(Level.INFO, "Process Shelved PID: " + pid);
+                } else {
+                    // We've previously seen the thread but not the process. Now we're seeing the process so just register it.
+                    assert !prevShelvedProcess.isMainVertexSet();
+                    prevShelvedProcess.setMainProcess(incomingVertex);
+                }
 
 
 
-			} else { // If its a thread
+            } else { // If its a thread
 
-				ShelvedProcess shelvedProcess = currentMainProcessNode.get(tgid);
+                ShelvedProcess shelvedProcess = currentMainProcessNode.get(tgid);
 
-				if ( shelvedProcess == null ) {
+                if (shelvedProcess == null) {
 
-					// hoping that we'll see the process sometime, so lets shelve this thread
-					shelvedProcess = new ShelvedProcess();
-					currentMainProcessNode.put(tgid, shelvedProcess );
-					return;
-				}
+                    // hoping that we'll see the process sometime, so lets shelve this thread
+                    shelvedProcess = new ShelvedProcess();
+                    currentMainProcessNode.put(tgid, shelvedProcess);
+                }
 
-				shelvedProcess.addAnotherVertexAnnotations(incomingVertex);
-				// logger.log(Level.INFO, "Thread Shelved PID: " + pid);
-			}
+                shelvedProcess.addAnotherVertexAnnotations(incomingVertex);
+                // logger.log(Level.INFO, "Thread Shelved PID: " + pid);
+            }
 
-		} else {
-			putInNextFilter(incomingVertex);
-		}
-	}
+        } else {
+            putInNextFilter(incomingVertex);
+        }
+    }
 
-	@Override
-	public void putEdge(AbstractEdge incomingEdge) {
+    @Override
+    public void putEdge(AbstractEdge incomingEdge) {
 
-		AbstractVertex sourceVertex = incomingEdge.getSourceVertex();
-		AbstractVertex destinationVertex = incomingEdge.getDestinationVertex();
-		
-		if (sourceVertex.type().equalsIgnoreCase("Process")) {
-			String pid = sourceVertex.getAnnotation("pid");
-			String tgid = sourceVertex.getAnnotation("tgid");
+        // logger.info("Shelving edge " + incomingEdge.toString());
+        shelvedEdges.add(incomingEdge);
 
-			if(!pid.equals(tgid)) {
-				// Map this thread to a flushed out vertex
-				String mappedPid = tgid;
-				if (currentMainProcessNode.containsKey(mappedPid) )
-					incomingEdge.setSourceVertex( currentMainProcessNode.get(mappedPid).getVertex() );
-				else
-					incomingEdge.setSourceVertex( flushedOutVertices.get(mappedPid) );
-			}
-		}
-		if (destinationVertex.type().equalsIgnoreCase("Process")) {
-			String pid = destinationVertex.getAnnotation("pid");
-			String tgid = destinationVertex.getAnnotation("tgid");
+    }
 
-			if(!pid.equals(tgid)) {
-				// Map this thread to a flushed out vertex
-				String mappedPid = tgid;
-				if (currentMainProcessNode.containsKey(mappedPid) )
-					incomingEdge.setDestinationVertex( currentMainProcessNode.get(mappedPid).getVertex() );
-				else
-					incomingEdge.setDestinationVertex( flushedOutVertices.get(mappedPid) );
-			}
-		}
+    /*
+     * End-Of-Events : Finish processing and flush everything pendning
+     */
+    public void EOE() {
 
-		// logger.info("Shelving edge " + incomingEdge.toString());
-		shelvedEdges.add(incomingEdge);
+        logger.log(Level.INFO, "EOE received. Ending stream ");
 
-	}
+        for (String pid : currentMainProcessNode.keySet()) {
 
-	/*
-	 * End-Of-Events : Finish processing and flush everything pendning
-	*/
-	public void EOE() {
+            logger.log(Level.INFO, "Flushing vertex: " + pid);
 
-		logger.log(Level.INFO, "EOE received. Ending stream ");
+            if (pid.equals("492")) {
+                logger.log(Level.INFO, "Processing 492 now");
+            }
 
-		for (String pid: currentMainProcessNode.keySet() )
-			flushVertex(pid);
+            if (!flushVertex(pid)) {
 
-		logger.log(Level.INFO, "Flushed edges " + Integer.toString(shelvedEdges.size()) );
+                ShelvedProcess shelvedProcess = currentMainProcessNode.get(pid);
 
-		for (AbstractEdge e: shelvedEdges ) {
+                if (!shelvedProcess.isMainVertexSet()) {
+                    // The main process for a thread or set of threads was not received
+                    // so just add a new dummy node for it 
+                    // so that shelved edges will correctly map to a node when flushing out
+                	
+                	logger.log(Level.WARNING, "Main Process of Thread Group " + pid + " was not set. Adding artificial vertex.");
+                	Process p = new Process();
+                    p.addAnnotation("pid", pid);
+                    currentMainProcessNode.get(pid).setMainProcess(p);
+                    
+                    if(!flushVertex(pid)) {
+                    	logger.log(Level.WARNING, "Something went wrong flushing Thread Group ID " + pid + ". Continuing with rest.");
+                    }
+                } 
+                
 
-			putInNextFilter(e);
-		}
-	}
+            }
+        }
 
-	private boolean flushVertex(String pid) {
 
-		// Flush previously shelved vertices, if any
+        logger.log(Level.INFO, "Flushing edges " + Integer.toString(shelvedEdges.size()));
 
-		ShelvedProcess previousProcess = currentMainProcessNode.get(pid);
-		if (previousProcess != null && previousProcess.isMainVertexSet() ) {
-			putInNextFilter( previousProcess.getVertex() );
-			return true;
-		}
-		return false;
+        for (AbstractEdge incomingEdge : shelvedEdges) {
 
-	}
+            AbstractVertex sourceVertex = incomingEdge.getSourceVertex();
+            AbstractVertex destinationVertex = incomingEdge.getDestinationVertex();
 
+
+            if (sourceVertex.type().equalsIgnoreCase("Process")) {
+                String pid = sourceVertex.getAnnotation("pid");
+                String tgid = sourceVertex.getAnnotation("tgid");
+
+                if (!pid.equals(tgid)) {
+                    // Map this thread to a flushed out vertex
+                    String mappedPid = tgid;
+
+                    logger.log(Level.INFO, "Remapping " + mappedPid + " to " + tgid);
+                    if (currentMainProcessNode.containsKey(mappedPid)) {
+                        incomingEdge.setSourceVertex(currentMainProcessNode.get(mappedPid).getVertex());
+                    } else {
+                        incomingEdge.setSourceVertex(flushedOutVertices.get(mappedPid));
+                    }
+                }
+            }
+            if (destinationVertex.type().equalsIgnoreCase("Process")) {
+                String pid = destinationVertex.getAnnotation("pid");
+                String tgid = destinationVertex.getAnnotation("tgid");
+
+                if (!pid.equals(tgid)) {
+                    // Map this thread to a flushed out vertex
+                    String mappedPid = tgid;
+                    if (currentMainProcessNode.containsKey(mappedPid)) {
+                        incomingEdge.setDestinationVertex(currentMainProcessNode.get(mappedPid).getVertex());
+                    } else {
+                        incomingEdge.setDestinationVertex(flushedOutVertices.get(mappedPid));
+                    }
+                }
+            }
+
+            putInNextFilter(incomingEdge);
+        }
+    }
+
+    private boolean flushVertex(String pid) {
+
+        // Flush previously shelved vertices, if any
+
+        ShelvedProcess previousProcess = currentMainProcessNode.get(pid);
+        if (previousProcess != null && previousProcess.isMainVertexSet()) {
+            putInNextFilter(previousProcess.getVertex());
+            return true;
+        }
+        return false;
+
+    }
 }
