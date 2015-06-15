98776ddb59fc93730f6cb05c8f66d39c3dd6e9f5
Hasanat Kazmi <hasanat.kazmi@sri.com>
2015-06-15 16:44:31 -0700

    Modified spade-controller.sh and spade-start.sh to stop writing nohup file and printing directory change info

--- bin/spade-controller.sh
+++ bin/spade-controller.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 SPADE_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/../ && pwd )"
-pushd ${SPADE_ROOT}
+pushd ${SPADE_ROOT} > /dev/null
 java -cp './build:./lib/*' spade.client.ControlClient
-popd
+popd > /dev/null
--- bin/spade-start.sh
+++ bin/spade-start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 SPADE_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/../ && pwd )"
-pushd ${SPADE_ROOT}
-nohup java -cp "./build:./lib/*" -server -Xms128M -Xmx512M spade.core.Kernel &
-popd
+pushd ${SPADE_ROOT} > /dev/null
+nohup java -cp "./build:./lib/*" -server -Xms128M -Xmx512M spade.core.Kernel > /dev/null 2>&1 &
+popd > /dev/null
