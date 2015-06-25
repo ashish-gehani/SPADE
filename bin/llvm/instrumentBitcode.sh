1d86b510a60018a39d100f0f82e6d97b6362fb48
hashim sharif <hashim.sharif91@gmail.com>
2015-06-24 21:42:05 -0500

    Cleaning up LLVMTrace code

--- bin/llvm/instrumentBitcode.sh
+++ bin/llvm/instrumentBitcode.sh
@@ -1,7 +1,7 @@
 llvm-link $1 flush.bc -o linked.bc
 if [$2 != "-no-monitor"]; then
 	opt -dot-callgraph linked.bc -o callgraph.bc	
-	java -cp ../src  spade/filter/LLVMFilter callgraph.dot $2 functionsOut
+	java -cp ../build  spade/filter/FunctionMonitor callgraph.dot $2 functionsOut
 	opt -load ./LLVMTrace.so -provenance -FunctionNames-input functionsOut linked.bc -o $3 
 else
 	opt -load ./LLVMTrace.so -provenance -FunctionNames-input "-no-monitor" linked.bc -o $3 
