8047efe5ca712825724c378883ab9688fbd82363
hashim sharif <hashim.sharif91@gmail.com>
2015-06-26 14:20:57 -0700

    Adding llvm build to Makefile.in, Removing buildTrace.sh. Add paths adjusted in llvmTrace.sh

--- bin/llvm/llvmTrace.sh
+++ bin/llvm/llvmTrace.sh
@@ -4,15 +4,15 @@ LLVM_SOURCE=$1
 FUNCTION_FILE=$2
 LLVM_TARGET=$3
 SRC_PATH=../../src
-LLC=llc-3.6
+LLC=llc
 CC=gcc
 
-$CC -static ${REPLIB_OSFLAG} ${SRC_PATH}/spade/reporter/LLVMTrace/llvmBridge.c -c -o ${SRC_PATH}/spade/reporter/LLVMTrace/llvmBridge.o 
+$CC -static ${REPLIB_OSFLAG} ${SRC_PATH}/spade/reporter/llvm/llvmBridge.c -c -o ${SRC_PATH}/spade/reporter/llvm/llvmBridge.o 
 bash instrumentBitcode.sh ${LLVM_SOURCE}.bc ${FUNCTION_FILE} ${LLVM_TARGET}.bc
 $LLC -relocation-model=pic ${LLVM_TARGET}.bc -o ${LLVM_TARGET}.s
 
-$CC -fPIC ${SRC_PATH}/spade/reporter/LLVMTrace/llvmClose.c -c -o ${SRC_PATH}/spade/reporter/LLVMTrace/llvmClose.o 
+$CC -fPIC ${SRC_PATH}/spade/reporter/llvm/llvmClose.c -c -o ${SRC_PATH}/spade/reporter/llvm/llvmClose.o 
 $CC ${LLVM_TARGET}.s -c -o ${LLVM_TARGET}.o
-$CC ${LLVM_TARGET}.o ${SRC_PATH}/spade/reporter/LLVMTrace/llvmClose.o -shared -o ${LLVM_TARGET}.so 
-$CC ${LLVM_TARGET}.so ${SRC_PATH}/spade/reporter/LLVMTrace/llvmBridge.o -o ${LLVM_TARGET} -Wl,-R -Wl,./ -lcrypt 
+$CC ${LLVM_TARGET}.o ${SRC_PATH}/spade/reporter/llvm/llvmClose.o -shared -o ${LLVM_TARGET}.so 
+$CC ${LLVM_TARGET}.so ${SRC_PATH}/spade/reporter/llvm/llvmBridge.o -o ${LLVM_TARGET} -Wl,-R -Wl,./ -lcrypt 
 
