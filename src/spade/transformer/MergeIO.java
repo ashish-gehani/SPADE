c113f7a5fafe07a5534fac96651c8d903d3ba669
hassaanirshad <hassaanirshad88@gmail.com>
2016-01-12 19:06:57 -0800

    Renamed transformer MergeIOEdges to MergeIO

--- src/spade/transformer/MergeIO.java
+++ src/spade/transformer/MergeIO.java
@@ -24,7 +24,7 @@ import spade.core.AbstractTransformer;
 import spade.core.DigQueryParams;
 import spade.core.Graph;
 
-public class MergeIOEdges extends AbstractTransformer{
+public class MergeIO extends AbstractTransformer{
 
 	public Graph putGraph(Graph graph, DigQueryParams digQueryParams){
 		Graph resultGraph = new Graph();
