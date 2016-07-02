8e80b497d619a842a27fa7321d476d48876a7a53
Ashish Gehani <ashish.gehani@sri.com>
2016-07-02 08:45:17 -0700

    Update LogManager.java

--- src/spade/utility/LogManager.java
+++ src/spade/utility/LogManager.java
@@ -2,7 +2,7 @@ package spade.utility;
 
 public class LogManager extends java.util.logging.LogManager{
 
-    /* Super class uses this in its callback when the system is
+    /* Super class uses reset() in its callback when the system is
      * shutdown. To avoid it being reset before we are done logging, we
      * redefine it to do nothing. We then call shutdownReset() at the end
      * of our shutdown processing, to call the super class's reset().
