48b5ce32df1d195c90b83d05d5d9b58b44cd23f7
Carolina de Senne Garcia <carol@visnet-117.csl.sri.com>
2017-07-18 17:36:00 -0700

    adding update function

--- src/spade/utility/InconsistencyDetector.java
+++ src/spade/utility/InconsistencyDetector.java
@@ -9,6 +9,7 @@ import spade.core.Vertex;
 import spade.core.Edge;
 import spade.core.Graph;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -20,26 +21,48 @@ import java.util.List;
 
 public class InconsistencyDetector {
 	
-	private Set<Graph> groundGraphs;
-	private Set<Graph> testGraphs;
-	private Map<AbstractVertex,List<AbstractEdge>> outgoingGroundGraphEdges;
-	private Map<String,Graph> hostnameToGraph; // maybe set of graphs, since one host can generate more than one graph?
+	private Set<Graph> cachedGraphs;
+	private Set<Graph> responseGraphs;
+	private Map<AbstractVertex,List<AbstractEdge>> outgoingCachedGraphEdges;
+	private Map<AbstractVertex,List<AbstractEdge>> outgoingResponseGraphEdges;
 		
 	/**
 	 * Constructs a new Inconsistency Detector
 	 * 
-	
 	 */
 	public InconsistencyDetector() {
-		groundGraphs = new HashSet<Graph>();
-		outgoingGroundGraphEdges = new HashMap<AbstractVertex,List<AbstractEdge>>();
+		cachedGraphs = new HashSet<Graph>();
+		responseGraphs = new HashSet<Graph>();
+		cachedGraphs.add(new Graph());
+		outgoingCachedGraphEdges = new HashMap<AbstractVertex,List<AbstractEdge>>();
+		outgoingResponseGraphEdges = new HashMap<AbstractVertex,List<AbstractEdge>>();
 	}
 	
+	/**
+	 * Updates the cached graph by adding a checked response graph to it
+	 *  should be called after an inconsistency detection that returned false
+	 *  Updates the cached main graph by adding all the vertices and edges in responseGraphs set to it
+	 *  Updates the outgoingCachedGraphEdges mapping
+	 */
 	public void update() {
-		// TODO
-		// add testsGraphs to groundGraphs
-		// update outgoingGroundGraphEdges (if a list already existed just add the new edges to it)
-		// update hostnameToGraph
+		Graph mainCachedGraph = cachedGraphs.iterator().next();
+		for(Graph G: responseGraphs) {
+			Map<AbstractVertex,List<AbstractEdge>> outgoingGEdges = outgoingEdges(G);
+			for(AbstractVertex V: G.vertexSet()) {
+				// put new vertex in cached graph
+				mainCachedGraph.putVertex(V);
+				// update outgoingCachedGraphEdges
+				List<AbstractEdge> cachedEdgeList = outgoingCachedGraphEdges.get(V);
+				if(cachedEdgeList == null)
+					cachedEdgeList = new ArrayList<AbstractEdge>();
+				// if a list already existed just add the new edges to it
+				cachedEdgeList.addAll(outgoingGEdges.get(V));
+				outgoingCachedGraphEdges.put(V,cachedEdgeList);
+			}
+			for(AbstractEdge e: G.edgeSet()) {
+				mainCachedGraph.putEdge(e);
+			}			
+		}
 	}
 
 
@@ -49,37 +72,47 @@ public class InconsistencyDetector {
 	 * @return true if found inconsistency or false if not
 	 */
 	public boolean findInconsistency() {
-		for(Graph T: testGraphs) {
-			if(hostnameToGraph.containsKey(T.getHostName())) {
-				if(findInconsistency(hostnameToGraph.get(T.getHostName()),T))
+		for(Graph T: responseGraphs) {
+				if(findInconsistency(getCachedGraph(),T))
 					return true;
-			}
 		}
 		return false;
 	}
 	
 	/**
+	 * 
+	 * @return main cached graph in cachedGraphs set
+	 */
+	private Graph getCachedGraph() {
+		return cachedGraphs.iterator().next();
+	}
+
+	/**
 	 * Algorithm to detect basic inconsistencies between graphs G1 and G2
 	 * 
 	 * @return true if found inconsistency or false if not
 	 */
-	public boolean findInconsistency(Graph G1, Graph G2) {
-		Set<AbstractVertex> V1 = G1.vertexSet();
-		Set<AbstractVertex> V2 = G2.vertexSet();
-		Set<AbstractEdge> E2 = G1.edgeSet();
-		for(AbstractVertex x : V1) {
-			if(!V2.contains(x)) // x is in groundGraph and not in testGraph
+	public boolean findInconsistency(Graph reference, Graph test) {
+		SimpleDateFormat referenceTime = new SimpleDateFormat(reference.getComputeTime());
+		SimpleDateFormat testTime = new SimpleDateFormat(test.getComputeTime());	
+		// TODO there is no function to compare them
+		
+		Set<AbstractVertex> referenceVertexSet = reference.vertexSet();
+		Set<AbstractVertex> testVertexSet = test.vertexSet();
+		Set<AbstractEdge> testEdgeSet = test.edgeSet();
+		for(AbstractVertex x : testVertexSet) {
+			if(!referenceVertexSet.contains(x)) // x is not in ground
 				continue;
-			List<AbstractEdge> E = outgoingGroundGraphEdges.get(x);
-			if(E == null)
+			List<AbstractEdge> referenceEdgeSet = outgoingCachedGraphEdges.get(x);
+			if(referenceEdgeSet == null)
 				continue;
-			for(AbstractEdge e : E) {
-				if(!E2.contains(e)) { // x -e-> y and e is in groundGraph and NOT in testGraph
-					if(V2.contains(e.getParentVertex())) {// y is in testGraph
+			for(AbstractEdge e : referenceEdgeSet) {
+				if(!testEdgeSet.contains(e)) { // x -e-> y and e is in reference and NOT in test
+					if(testVertexSet.contains(e.getParentVertex())) {// y is in testGraph
 						System.out.println("Inconsistency Detected: missing edge");
 						return true;
 					}
-					if(x.getDepth() < G1.getMaxDepth()) {
+					if(x.getDepth() < reference.getMaxDepth()) {
 						System.out.println("Inconsistency Detected: missing edge and vertex");
 						return true;
 					}
@@ -87,8 +120,8 @@ public class InconsistencyDetector {
 			}
 		}
 		// verify if every vertex in the edges is in V2
-		for(AbstractEdge e : E2) {
-			if(!V2.contains(e.getChildVertex()) || !V2.contains(e.getParentVertex()))
+		for(AbstractEdge e : testEdgeSet) {
+			if(!testVertexSet.contains(e.getChildVertex()) || !testVertexSet.contains(e.getParentVertex()))
 				return true;
 		}
 		return false;
@@ -116,29 +149,29 @@ public class InconsistencyDetector {
 	}
 
 	
-	public void setTestGraph(Set<Graph> t) {
-		testGraphs = t;
+	public void setResponseGraph(Set<Graph> t) {
+		responseGraphs = t;
 	}
 	
 	/**
 	 * @return ground graph groundGraph
 	 */
-	public Set<Graph> getGroundGraph() {
-		return groundGraphs;
+	public Set<Graph> getCachedGraphs() {
+		return cachedGraphs;
 	}
 	
 	/**
 	 * @return Graph testGraph
 	 */
-	public Set<Graph> getNewGraph() {
-		return testGraphs;
+	public Set<Graph> getResponseGraph() {
+		return responseGraphs;
 	}
 	
 	/**
 	 * @return outgoingEdges for ground graph groundGraph
 	 */
-	public Map<AbstractVertex,List<AbstractEdge>> getoutgoingGroundGraphEdges() {
-		return outgoingGroundGraphEdges;
+	public Map<AbstractVertex,List<AbstractEdge>> getoutgoingCachedGraphEdges() {
+		return outgoingCachedGraphEdges;
 	}
 	
 }
