ea2bec7b6400b23aa3bddbf4d1c52cfb2410bc12
Raza Ahmad <sra0nasir@gmail.com>
2017-08-10 16:39:29 -0700

    Added correct bin and cfg folders

---
+++ bin/keys/generatekeys.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+KEYS_PRESENT=cfg/ssl/key.generation.done
+
+if [ -f $KEYS_PRESENT ] ; then
+	printf 'Keys present. Will not generate new ones.\n'
+	exit
+fi
+
+mkdir -p cfg/ssl
+
+# Generate client public/private key pair into private keystore
+echo Generating client public private key pair
+keytool -genkey -alias clientprivate -keystore cfg/ssl/client.private -storetype JKS -keyalg rsa -dname "CN=Your Name, OU=Your Organizational Unit, O=Your Organization, L=Your City, S=Your State, C=Your Country" -storepass private -keypass private
+
+# Generate server public/private key pair
+echo Generating server public private key pair
+keytool -genkey -alias serverprivate -keystore cfg/ssl/server.private -storetype JKS -keyalg rsa -dname "CN=Your Name, OU=Your Organizational Unit, O=Your Organization, L=Your City, S=Your State, C=Your Country" -storepass private -keypass private
+
+# Export client public key and import it into public keystore
+echo Generating client public key file
+keytool -export -alias clientprivate -keystore cfg/ssl/client.private -file cfg/ssl/temp.key -storepass private
+keytool -import -noprompt -alias clientpublic -keystore cfg/ssl/client.public -file cfg/ssl/temp.key -storepass public
+rm -f cfg/ssl/temp.key
+
+# Export server public key and import it into public keystore
+echo Generating server public key file
+keytool -export -alias serverprivate -keystore cfg/ssl/server.private -file cfg/ssl/temp.key -storepass private
+keytool -import -noprompt -alias serverpublic -keystore cfg/ssl/server.public -file cfg/ssl/temp.key -storepass public
+rm -f cfg/ssl/temp.key
+
+# Note that keys have been generated
+touch $KEYS_PRESENT
