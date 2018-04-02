f0128dc50f839cca93fdb444a69d93b63cad8a42
Hassaan Irshad <hassaanirshad88@gmail.com>
2018-04-02 12:38:27 -0700

    Makefile.in: Using javac instead of javac to be compatible with JDK10

--- src/spade/reporter/libMacFUSE.c
+++ src/spade/reporter/libMacFUSE.c
@@ -26,7 +26,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #define FUSE_USE_VERSION 26
 
-#include "libMacFUSE.h"
+#include "spade_reporter_MacFUSE.h"
 
 #define _GNU_SOURCE
 
