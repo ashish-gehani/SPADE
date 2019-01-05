c9f9686552c2e2ace1cfbb28beb2e32d10643f97
Hassaan Irshad <hassaanirshad88@gmail.com>
2019-01-05 13:29:01 -0800

    IORuns filter updates:
    1) Filter used to merge reads/writes based on artifact annotation 'location' only.
    2) Now the annotation has to be specified using the argument 'key'. Refer to wiki for usage.
    3) The default value for argument 'key' is 'path', defined in 'cfg/spade.filter.IORuns.config'.

--- src/spade/filter/IORuns.java
+++ src/spade/filter/IORuns.java
@@ -19,22 +19,50 @@
  */
 package spade.filter;
 
-import spade.core.AbstractEdge;
-import spade.core.AbstractFilter;
-import spade.core.AbstractVertex;
-import spade.edge.opm.Used;
-import spade.edge.opm.WasGeneratedBy;
-import spade.vertex.opm.Artifact;
-
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.Queue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import spade.core.AbstractEdge;
+import spade.core.AbstractFilter;
+import spade.core.AbstractVertex;
+import spade.edge.opm.Used;
+import spade.edge.opm.WasGeneratedBy;
+import spade.utility.CommonFunctions;
+import spade.utility.LoadableField;
+import spade.utility.LoadableFieldHelper;
+import spade.vertex.opm.Artifact;
+
+/**
+ * Takes in argument 'key' whose value can be comma-separated.
+ * 
+ * 'key' used to tell the filter about the annotations unique to artifacts based on which to merge
+ * consecutive reads, and writes.
+ * 
+ * Example: 'add filter IORuns position=1 key=path'
+ * The above-mentioned command merges reads/writes to artifacts (files) which have a unique value for annotation 'path'
+ * 
+ * Example: 'add filter IORuns position=1 key="remote address,remote port,local address,local port"'
+ * The above-mentioned command merges reads/writes to artifacts (network) which have a unique value for annotations
+ * 'remote address', 'remote port', 'local address', 'local port'.
+ * 
+ * The filter is applied to only those artifacts which contain all the above-mentioned annotations specified in arguments
+ * as 'key'.
+ */
 public class IORuns extends AbstractFilter {
 
-    private final String artifactKey = "location";
+	private static final Logger logger = Logger.getLogger(IORuns.class.getName());
+	
+	private static final String argNameArtifactKey = "key";
+	
+	@LoadableField(name=argNameArtifactKey, optional=false, splitBy=",")
+	private final String[] artifactsKeysArray = null;
+	
     private final Map<String, HashSet<String>> writes;
     private final Map<String, HashSet<String>> reads;
     private final Queue<AbstractVertex> vertexBuffer;
@@ -44,10 +72,78 @@ public class IORuns extends AbstractFilter {
         reads = new HashMap<>();
         vertexBuffer = new LinkedList<>();
     }
-
+    
+    private boolean printGlobals(){
+		try{
+			String globalsString = LoadableFieldHelper.allLoadableFieldsToString(this);
+			logger.log(Level.INFO, "Arguments: " + globalsString);
+			return true;
+		}catch(Exception e){
+			logger.log(Level.SEVERE, "Failed to log globals", e);
+			return false;
+		}
+	}
+    
+    private boolean initGlobals(String arguments){
+		Map<String, String> globalsMap = null;
+		try{
+			globalsMap = CommonFunctions.getGlobalsMapFromConfigAndArguments(this.getClass(), arguments);
+		}catch(Exception e){
+			logger.log(Level.SEVERE, "Failed to build globals map", e);
+			return false;
+		}
+		
+		try{
+			LoadableFieldHelper.loadAllLoadableFieldsFromMap(this, globalsMap);
+		}catch(Exception e){
+			logger.log(Level.SEVERE, "Exception in initializing globals", e);
+			return false;
+		}
+		
+		for(String artifactsKey : artifactsKeysArray){
+			if(artifactsKey.trim().isEmpty()){
+				logger.log(Level.SEVERE, "Empty value for argument '"+argNameArtifactKey+"' in array: " +
+						Arrays.asList(artifactsKeysArray));
+				return false;
+			}
+		}
+		
+		if(printGlobals()){
+			return true;
+		}else{
+			return false;
+		}
+	}
+    
+    @Override
+    public boolean initialize(String arguments){
+    	if(!initGlobals(arguments)){
+			return false;
+		}else{
+			return true;
+		}
+    }
+    
+    private boolean artifactContainsAllArtifactKeys(AbstractVertex artifact){
+    	for(String artifactKey : artifactsKeysArray){
+    		if(artifact.getAnnotation(artifactKey) == null){
+    			return false;
+    		}
+    	}
+    	return true;
+    }
+    
+    private String getArtifactKeysValues(AbstractVertex vertex){
+    	String value = "";
+    	for(String key : artifactsKeysArray){
+    		value += vertex.getAnnotation(key) + ",";
+    	}
+    	return value;
+    }
+    
     @Override
     public void putVertex(AbstractVertex incomingVertex) {
-        if ((incomingVertex instanceof Artifact) && (incomingVertex.getAnnotation(artifactKey) != null)) {
+        if ((incomingVertex instanceof Artifact) && artifactContainsAllArtifactKeys(incomingVertex)) {
             vertexBuffer.add(incomingVertex);
         } else {
             putInNextFilter(incomingVertex);
@@ -56,9 +152,9 @@ public class IORuns extends AbstractFilter {
 
     @Override
     public void putEdge(AbstractEdge incomingEdge) {
-        if ((incomingEdge instanceof Used) && (incomingEdge.getParentVertex().getAnnotation(artifactKey) != null)) {
+        if ((incomingEdge instanceof Used) && artifactContainsAllArtifactKeys(incomingEdge.getParentVertex())) {
             Used usedEdge = (Used) incomingEdge;
-            String fileVertexHash = usedEdge.getParentVertex().getAnnotation(artifactKey);
+            String fileVertexHash = getArtifactKeysValues(usedEdge.getParentVertex());
             String processVertexHash = Integer.toString(usedEdge.getChildVertex().hashCode());
             if (!reads.containsKey(fileVertexHash)) {
                 HashSet<String> tempSet = new HashSet<>();
@@ -80,9 +176,9 @@ public class IORuns extends AbstractFilter {
                 HashSet<String> tempSet = writes.get(fileVertexHash);
                 tempSet.remove(processVertexHash);
             }
-        } else if ((incomingEdge instanceof WasGeneratedBy) && (incomingEdge.getChildVertex().getAnnotation(artifactKey) != null)) {
+        } else if ((incomingEdge instanceof WasGeneratedBy) && artifactContainsAllArtifactKeys(incomingEdge.getChildVertex())){
             WasGeneratedBy wgb = (WasGeneratedBy) incomingEdge;
-            String fileVertexHash = wgb.getChildVertex().getAnnotation(artifactKey);
+            String fileVertexHash = getArtifactKeysValues(wgb.getChildVertex());
             String processVertexHash = Integer.toString(wgb.getParentVertex().hashCode());
             if (!writes.containsKey(fileVertexHash)) {
                 HashSet<String> tempSet = new HashSet<>();
