a80cee6b7a6c594e86241dad2a031e7429df395d
Hassaan Irshad <hassaanirshad88@gmail.com>
2019-04-18 21:03:58 -0700

    netio module: Handling more of 'kill' syscall arguments

--- src/spade/reporter/audit/kernel-modules/netio.c
+++ src/spade/reporter/audit/kernel-modules/netio.c
@@ -456,7 +456,17 @@ asmlinkage long new_kill(pid_t pid, int sig){
 	
 	if(stop == 0){
 		if(usingKey == 1){
-			int tgid = get_tgid(pid);
+			int checkPid;
+			if(pid < -1){
+				checkPid = pid * -1;
+			}else if(pid == 0){
+				checkPid = current_task->pid;
+			}else if(pid == -1){
+				checkPid = pid;
+			}else{
+				checkPid = pid;
+			}
+			int tgid = get_tgid(checkPid);
 			if(exists_in_array(tgid, harden_tgids, harden_tgids_len) == 1){
 				// don't kill
 				return -1;
