6b1c79c1110a0622e78e4ff0fb6584c9aff4e334
hassaanirshad <hassaanirshad88@gmail.com>
2025-06-22 22:34:30 +0000

    Add class AmebaAppState

---
+++ src/spade/reporter/audit/bpf/AmebaAppState.java
@@ -0,0 +1,62 @@
+/*
+ --------------------------------------------------------------------------------
+ SPADE - Support for Provenance Auditing in Distributed Environments.
+ Copyright (C) 2025 SRI International
+
+ This program is free software: you can redistribute it and/or
+ modify it under the terms of the GNU General Public License as
+ published by the Free Software Foundation, either version 3 of the
+ License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------------
+ */
+
+package spade.reporter.audit.bpf;
+
+public enum AmebaAppState {
+    
+    APP_STATE_STARTING("APP_STATE_STARTING"),
+    APP_STATE_OPERATIONAL("APP_STATE_OPERATIONAL"),
+    APP_STATE_OPERATIONAL_PID("APP_STATE_OPERATIONAL_PID"),
+    APP_STATE_OPERATIONAL_WITH_ERROR("APP_STATE_OPERATIONAL_WITH_ERROR"),
+    APP_STATE_STOPPED_WITH_ERROR("APP_STATE_STOPPED_WITH_ERROR"),
+    APP_STATE_STOPPED_NORMALLY("APP_STATE_STOPPED_NORMALLY");
+
+    private final String value;
+
+    AmebaAppState(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public boolean isStopped() {
+        return (
+            this == APP_STATE_STOPPED_WITH_ERROR
+            || this == APP_STATE_STOPPED_NORMALLY
+        );
+    }
+
+    private static final java.util.Map<String, AmebaAppState> STRING_TO_ENUM =
+        new java.util.HashMap<>();
+
+    static {
+        for (AmebaAppState s : values()) {
+            STRING_TO_ENUM.put(s.value, s);
+        }
+    }
+
+    public static AmebaAppState fromString(String value) {
+        return STRING_TO_ENUM.get(value);
+    }
+
+}
