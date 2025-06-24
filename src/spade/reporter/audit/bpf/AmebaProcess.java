3c02b6b30e4ee709f529648a92df4a44ebc145ce
hassaanirshad <hassaanirshad88@gmail.com>
2025-06-24 00:54:45 +0000

    AmebaProcess cleanup

--- src/spade/reporter/audit/bpf/AmebaProcess.java
+++ src/spade/reporter/audit/bpf/AmebaProcess.java
@@ -23,8 +23,6 @@ package spade.reporter.audit.bpf;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
