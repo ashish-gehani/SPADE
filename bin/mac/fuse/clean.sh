4cb23e122b5479237bb41ebd63f2d6c064542434
hassaanirshad <hassaanirshad88@gmail.com>
2026-04-28 22:56:30 +0000

    Add build clean script for mac fuse

---
+++ bin/mac/fuse/clean.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# SPADE - Support for Provenance Auditing in Distributed Environments.
+# Copyright (C) 2026 SRI International.
+
+set -e
+
+SPADE_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/../../.. && pwd )"
+SPADE_LIB="${SPADE_ROOT}/lib"
+SPADE_SRC="${SPADE_ROOT}/src"
+
+rm -f "${SPADE_SRC}/spade/reporter/spade_reporter_MacFUSE.h"
+rm -f "${SPADE_LIB}"/libMacFUSE.*
