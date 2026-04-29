652f8ef54065ed72cf3c42d19b6761b50bc8e22a
hassaanirshad <hassaanirshad88@gmail.com>
2026-04-29 14:59:18 +0000

    Update android/build.sh to not require env

--- bin/android/build.sh
+++ bin/android/build.sh
@@ -4,28 +4,101 @@
 # Copyright (C) 2026 SRI International.
 
 
-source "$( dirname "${BASH_SOURCE[0]}" )/../env.sh"
 
-JAVAC_CP="$("${JAVAC_CP_PATH}")"
-JAVAC="$(which javac)"
+JAVAC=""
+SPADE_BUILD=""
+ANDROID_BUILD=""
+JAVAC_OPTIONS=""
+JAVA_SRC=""
 
-DX="$(which dx 2>/dev/null)"
-if [[ -z "${DX}" ]]; then
-    echo "Error: dx not found. Please install Android SDK build tools."
-    exit 1
-fi
-ANDROID_BUILD_TOOLS="$(dirname "${DX}")/"
 
-mkdir -p "${SPADE_BUILD}"
-${JAVAC} ${JAVAC_OPTIONS} -cp "${JAVAC_CP}" -sourcepath src -d "${SPADE_BUILD}" src/spade/client/Android.java
-${JAVAC} ${JAVAC_OPTIONS} -cp "${JAVAC_CP}" -sourcepath src -d "${SPADE_BUILD}" src/spade/core/*.java
-${JAVAC} ${JAVAC_OPTIONS} -cp "${JAVAC_CP}" -sourcepath src -d "${SPADE_BUILD}" src/spade/utility/*.java
-${JAVAC} ${JAVAC_OPTIONS} -cp "${JAVAC_CP}" -sourcepath src -d "${SPADE_BUILD}" src/spade/filter/IORuns.java
-${JAVAC} ${JAVAC_OPTIONS} -cp "${JAVAC_CP}" -sourcepath src -d "${SPADE_BUILD}" src/spade/storage/Graphviz.java
-${JAVAC} ${JAVAC_OPTIONS} -cp "${JAVAC_CP}" -sourcepath src -d "${SPADE_BUILD}" src/spade/reporter/Strace.java
-# mkdir -p android-lib
-# ${ANDROID_BUILD_TOOLS}/dx --dex --output=android-lib/lucene-core-3.5.0.jar lib/lucene-core-3.5.0.jar
-mkdir -p "${ANDROID_BUILD}"
-echo "dalvikvm -cp android-spade.jar spade.client.Android" > "${ANDROID_BUILD}/control.sh"
-cd "${SPADE_BUILD}"
-${ANDROID_BUILD_TOOLS}/dx --dex --verbose --no-strict --output="${ANDROID_BUILD}/android-spade.jar" spade
+print_help() {
+    echo "Usage: $(basename "$0") --javac <path> --spade_build <path> --android_build <path> --javac_options <opts> --java_src <path>"
+    echo ""
+    echo "Options:"
+    echo "    --javac <path>            Path to the javac compiler"
+    echo "    --spade_build <path>      Path to the SPADE build output directory"
+    echo "    --android_build <path>    Path to the Android build output directory"
+    echo "    --javac_options <opts>    Additional javac options"
+    echo "    --java_src <path>         Path to the Java source directory"
+    echo "    --help                    Show this message and exit"
+    exit 0
+}
+
+parse_args() {
+    while [[ $# -gt 0 ]]; do
+        case "$1" in
+            --javac)         JAVAC="$2";          shift 2 ;;
+            --spade_build)   SPADE_BUILD="$2";    shift 2 ;;
+            --android_build) ANDROID_BUILD="$2";  shift 2 ;;
+            --javac_options) JAVAC_OPTIONS="$2";  shift 2 ;;
+            --java_src)      JAVA_SRC="$2";       shift 2 ;;
+            --help)          print_help ;;
+            *) echo "Unknown argument: $1"; print_help ;;
+        esac
+    done
+}
+
+validate_args() {
+    if [[ ! -x "${JAVAC}" ]]; then
+        echo "Error: --javac '${JAVAC}' is not executable"
+        exit 1
+    fi
+    if [[ -z "${SPADE_BUILD}" ]]; then
+        echo "Error: --spade_build is required"
+        exit 1
+    fi
+    if [[ -z "${ANDROID_BUILD}" ]]; then
+        echo "Error: --android_build is required"
+        exit 1
+    fi
+    if [[ -z "${JAVAC_OPTIONS}" ]]; then
+        echo "Error: --javac_options is required"
+        exit 1
+    fi
+    if [[ -z "${JAVA_SRC}" ]]; then
+        echo "Error: --java_src is required"
+        exit 1
+    fi
+}
+
+javac_compile() {
+    ${JAVAC} ${JAVAC_OPTIONS} -sourcepath "${JAVA_SRC}" -d "${SPADE_BUILD}" "$@"
+}
+
+# TODO: compilation should not be done here
+compile() {
+    mkdir -p "${SPADE_BUILD}"
+    javac_compile "${JAVA_SRC}"/spade/client/Android.java
+    javac_compile "${JAVA_SRC}"/spade/core/*.java
+    javac_compile "${JAVA_SRC}"/spade/utility/*.java
+    javac_compile "${JAVA_SRC}"/spade/filter/IORuns.java
+    javac_compile "${JAVA_SRC}"/spade/storage/Graphviz.java
+    javac_compile "${JAVA_SRC}"/spade/reporter/Strace.java
+}
+
+package() {
+    local dx
+    local android_build_tools
+    dx="$(which dx 2>/dev/null)"
+    if [[ -z "${dx}" ]]; then
+        echo "Error: dx not found. Please install Android SDK build tools."
+        exit 1
+    fi
+    android_build_tools="$(dirname "${dx}")/"
+    # mkdir -p android-lib
+    # ${android_build_tools}/dx --dex --output=android-lib/lucene-core-3.5.0.jar lib/lucene-core-3.5.0.jar
+    mkdir -p "${ANDROID_BUILD}"
+    echo "dalvikvm -cp android-spade.jar spade.client.Android" > "${ANDROID_BUILD}/control.sh"
+    cd "${SPADE_BUILD}"
+    ${android_build_tools}/dx --dex --verbose --no-strict --output="${ANDROID_BUILD}/android-spade.jar" spade
+}
+
+main() {
+    parse_args "$@"
+    validate_args
+    compile
+    package
+}
+
+main "$@"
