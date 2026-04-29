e752ac96ad1e390a1ffe010f80f6ff461cc905ef
hassaanirshad <hassaanirshad88@gmail.com>
2026-04-29 15:36:49 +0000

    Move common kernel module vars to Makefile

--- bin/linux/audit/kernel/build.sh
+++ bin/linux/audit/kernel/build.sh
@@ -3,39 +3,67 @@
 # SPADE - Support for Provenance Auditing in Distributed Environments.
 # Copyright (C) 2026 SRI International.
 
-set -e
 
-source "$( dirname "${BASH_SOURCE[0]}" )/../../../env.sh"
+# globals
+KERNEL_MODULES_DEBUG=""
+KERNEL_MODULES_SRC=""
+KERNEL_MODULES_INSTALL=""
 
-KERNEL_MODULES_DEBUG=false
 
-function parse_args(){
+print_help() {
+    echo "Usage: $(basename "$0") --kernel_modules_src <path> --kernel_modules_install <path> --kernel_modules_debug <true|false>"
+    echo ""
+    echo "Options:"
+    echo "    --kernel_modules_src <path>         Path to kernel modules source directory"
+    echo "    --kernel_modules_install <path>     Path to kernel modules install directory"
+    echo "    --kernel_modules_debug <true|false> Enable debug build (default: false)"
+    echo "    --help                              Show this message and exit"
+    exit 0
+}
+
+parse_args() {
     while [[ $# -gt 0 ]]; do
         case "$1" in
-            --kernel-modules-debug)
-                KERNEL_MODULES_DEBUG="$2"
-                shift 2
-                ;;
-            *)
-                echo "Unknown argument: $1"
-                exit 1
-                ;;
+            --kernel_modules_src)     KERNEL_MODULES_SRC="$2";     shift 2 ;;
+            --kernel_modules_install) KERNEL_MODULES_INSTALL="$2"; shift 2 ;;
+            --kernel_modules_debug)   KERNEL_MODULES_DEBUG="$2";   shift 2 ;;
+            --help)                   print_help ;;
+            *) echo "Unknown argument: $1"; print_help ;;
         esac
     done
 }
 
-function main(){
-    parse_args "$@"
-
-    if [[ "${KERNEL_MODULES_DEBUG}" == "true" ]]; then
-        KERNEL_MODULES_BUILD_TARGET="debug"
-    else
-        KERNEL_MODULES_BUILD_TARGET="release"
+validate_args() {
+    if [[ -z "${KERNEL_MODULES_SRC}" ]]; then
+        echo "Error: --kernel_modules_src is required"
+        exit 1
     fi
+    if [[ -z "${KERNEL_MODULES_INSTALL}" ]]; then
+        echo "Error: --kernel_modules_install is required"
+        exit 1
+    fi
+    if [[ -z "${KERNEL_MODULES_DEBUG}" ]]; then
+        echo "Error: --kernel_modules_debug is required"
+        exit 1
+    fi
+}
 
-    make --no-print-directory -C "${AUDIT_KERNEL_MODULES_SRC}" \
-        INSTALL_DIR="${AUDIT_KERNEL_MODULES_INSTALL}" \
-        "${KERNEL_MODULES_BUILD_TARGET}" install
+build() {
+    local target
+    if [[ "${KERNEL_MODULES_DEBUG}" == "true" ]]; then
+        target="debug"
+    else
+        target="release"
+    fi
+    make --no-print-directory -C "${KERNEL_MODULES_SRC}" \
+        INSTALL_DIR="${KERNEL_MODULES_INSTALL}" \
+        "${target}" install
+}
+
+main() {
+    parse_args "$@"
+    validate_args
+    build
 }
 
 main "$@"
--- bin/linux/audit/kernel/clean.sh
+++ bin/linux/audit/kernel/clean.sh
@@ -3,14 +3,54 @@
 # SPADE - Support for Provenance Auditing in Distributed Environments.
 # Copyright (C) 2026 SRI International.
 
-set -e
 
-source "$( dirname "${BASH_SOURCE[0]}" )/../../../env.sh"
+# globals
+KERNEL_MODULES_SRC=""
+KERNEL_MODULES_INSTALL=""
 
-function main(){
-    make --no-print-directory -C "${AUDIT_KERNEL_MODULES_SRC}" \
-        INSTALL_DIR="${AUDIT_KERNEL_MODULES_INSTALL}" \
+
+print_help() {
+    echo "Usage: $(basename "$0") --kernel_modules_src <path> --kernel_modules_install <path>"
+    echo ""
+    echo "Options:"
+    echo "    --kernel_modules_src <path>       Path to kernel modules source directory"
+    echo "    --kernel_modules_install <path>   Path to kernel modules install directory"
+    echo "    --help                            Show this message and exit"
+    exit 0
+}
+
+parse_args() {
+    while [[ $# -gt 0 ]]; do
+        case "$1" in
+            --kernel_modules_src)     KERNEL_MODULES_SRC="$2";     shift 2 ;;
+            --kernel_modules_install) KERNEL_MODULES_INSTALL="$2"; shift 2 ;;
+            --help)                   print_help ;;
+            *) echo "Unknown argument: $1"; print_help ;;
+        esac
+    done
+}
+
+validate_args() {
+    if [[ -z "${KERNEL_MODULES_SRC}" ]]; then
+        echo "Error: --kernel_modules_src is required"
+        exit 1
+    fi
+    if [[ -z "${KERNEL_MODULES_INSTALL}" ]]; then
+        echo "Error: --kernel_modules_install is required"
+        exit 1
+    fi
+}
+
+clean() {
+    make --no-print-directory -C "${KERNEL_MODULES_SRC}" \
+        INSTALL_DIR="${KERNEL_MODULES_INSTALL}" \
         clean
+}
+
+main() {
+    parse_args "$@"
+    validate_args
+    clean
 }
 
 main "$@"
