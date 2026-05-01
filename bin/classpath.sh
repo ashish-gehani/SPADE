370cc5a2d41f933d5cd2297793997c31aa43aa04
hassaanirshad <hassaanirshad88@gmail.com>
2026-05-01 16:21:05 +0000

    Update classpath script to use maven

--- bin/classpath.sh
+++ bin/classpath.sh
@@ -1,47 +1,18 @@
 #!/bin/bash
 
 spade_home_path="$(cd "$( dirname "${BASH_SOURCE[0]}" )"/../ && pwd)"
-spade_cfg_path="${spade_home_path}/cfg"
-spade_classpath_file="${spade_cfg_path}/java.classpath"
-
-if [ ! -f "${spade_classpath_file}" ]
-then
-  echo "Missing classpath entries file: ${spade_classpath_file}"
-  exit 1
-fi
 
 os_name=$(uname)
 
-is_cygwin=0
-
-# default for all non-cygwin
 classpath_separator=":"
 
 if [[ "${os_name}" = *"CYGWIN"* ]]
 then
-  # update SPADE home path to windows format
   spade_home_path=`cygpath -w $spade_home_path`
   classpath_separator=";"
-  is_cygwin=1
 fi
 
-classpath_str=""
+deps=$(cd "${spade_home_path}" && mvn -q --no-transfer-progress dependency:build-classpath \
+    -Dmdep.outputFile=/dev/stdout 2>/dev/null)
 
-while IFS= read -r line
-do
-  path_with_spade_home=""
-  if echo "$line" | grep -q "^/"
-  then
-    path_with_spade_home="${line}"
-  else
-    path_with_spade_home="${spade_home_path}/${line}"
-  fi
-  classpath_str="${classpath_str}${path_with_spade_home}${classpath_separator}"
-done < "${spade_classpath_file}"
-
-if [ "$is_cygwin" -eq 1 ]
-then
-  classpath_str="${classpath_str//\//\\\\}"
-fi
-
-echo "${classpath_str::${#classpath_str}-1}"
+echo "${spade_home_path}/build${classpath_separator}${deps}"
